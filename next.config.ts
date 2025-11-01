aae1aa473e91e467e0639d0d37af934442b9bf58
Usenobong Usen Etokakpan <jabulanietokakpan@gmail.com>
2025-11-01 12:02:10 +0100

    disabling pwa

--- next.config.ts
+++ next.config.ts
@@ -55,5 +55,5 @@ const nextConfig: NextConfig = {
 export default withPWA({
   dest: 'public',
   register: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: true, // Disabled to prevent cache issues
 })(nextConfig);
